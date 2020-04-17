5159ebbc3ee0edf940b70874a9c386f8554d51f7
Ichwan Nur Dwi Yoga <ichwannurdwiyoga@hotmail.com>
2020-04-17 21:00:21 +0700

    Commit Pertama

---
+++ src/ch02/Produk.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch02;
+
+/**
+ *
+ * @author ichwannur
+ */
+import java.util.Date;
+public class Produk {
+    String barcode, nama;
+	Date tanggalKedaluarsa;
+	double harga;
+
+	public String getBarcode(){
+		return barcode;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public Date getTanggalKedaluarsa(){
+		return tanggalKedaluarsa;
+	}
+
+	public double getHarga(){
+		return harga;
+	}
+}
